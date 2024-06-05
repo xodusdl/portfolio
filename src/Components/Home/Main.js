1ebfefd9d582fedd190fd9fa4c88be1fb52c37e6
kim <rlaxosl03@gmail.com>
2024-06-06 00:12:53 +0900

    eleventh

--- src/Components/Home/Main.js
+++ src/Components/Home/Main.js
@@ -5,10 +5,16 @@ export default function Main() {
     return (
         <div id={Style.main_wrap}>
             <section id={Style.main}>
-                <p className={Style.logo}>TAEYEON</p>
+                <p className={Style.logo}>Taeyeon Kim</p>
                 <p className={Style.text}>Got A Project In Mine?</p>
-                <p className={Style.text_bottom}>LET'S TALK</p>
-                <div className={Style.arrow}><img src='./images/arrow.svg' alt='arrow'/></div>
+                <p className={Style.text_bottom}>Portfolio</p>
+                <div className={Style.arrow}>
+                    <img src='./images/arrow.svg' alt='arrow'/>
+                    <img src='./images/arrow.svg' alt='arrow'/>
+                    <img src='./images/arrow.svg' alt='arrow'/>
+                    {/* <p class="arrow_img"></p> */}
+                    {/* <p class="arrow_img"><img src='./images/arrow.svg' alt='arrow'/></p> */}
+                </div>
             </section>
         </div>
       )
