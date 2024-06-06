557ff17b42a94d3176c15a3b1201052ec0e73bb7
kim <rlaxosl03@gmail.com>
2024-06-06 20:09:08 +0900

    13

--- src/Pages/WebList.js
+++ src/Pages/WebList.js
@@ -17,7 +17,6 @@ export default function WebList() {
 
   const [ allList ] = useList()
 
-
   return (
     <div id={Style.web_wrap}>
       <section id={Style.list}>
@@ -44,55 +43,7 @@ export default function WebList() {
                   </li>
                 )
               })
-
             }
-
-            {/* 
-
-            <li>
-              <div className={Style.portfolio_text_box}>
-              <p className={Style.info_title}>seoulbicycle (개인)</p>
-                <p className={Style.keyword}>적응형, React<br/>2024.02.12 ~ 2024.02.29</p>
-                <p className={Style.info_text}>자전거 대여 사이트를 주제로 제작한 React 프로젝트입니다.</p>
-              </div>
-              <div className={Style.img}>
-                <img src='./images/seoul.jpg' alt='seoulbicycle'/>
-                <div className={Style.link_box} onClick={()=> openTab ("https://rentalbicycle.netlify.app/")}><p>Link to</p></div>
-              </div>
-            </li>
-            <li>
-              <div className={Style.portfolio_text_box}>
-                <p className={Style.info_title}>zarakids (개인)<span onClick={()=> openTab ("https://www.figma.com/file/zha4Wy1xndQyJRqYMBukHT/zarakids_Figma?type=design&node-id=0%3A1&mode=design&t=qZNu4iDS8ReCJOKA-1")}><FontAwesomeIcon icon={faFigma} /></span></p>
-                <p className={Style.keyword}>반응형, React<br/>2024.03.01 ~ 2024.03.15</p>
-                <p className={Style.info_text}>의류 브랜드 ZARA 홈페이지를 React 활용하여 리뉴얼하였습니다.</p>
-              </div>
-              <div className={Style.img}>
-                <img src='./images/zara.jpg' alt='zarakids'/>
-                <div className={Style.link_box} onClick={()=> openTab ("https://zarakidsclothes.netlify.app/")}><p>Link to</p></div>
-              </div>
-            </li>
-            <li>
-              <div className={Style.portfolio_text_box}>
-                <p className={Style.info_title}>Glossier (개인)</p>
-                <p className={Style.keyword}>html, css<br/>2024.04.29 ~ 2024.05.03</p>
-                <p className={Style.info_text}>Glossier 홈페이지를 html, css를 활용하여 제작하였습니다.</p>
-              </div>
-              <div className={Style.img}>
-                <img src='./images/glossier.jpg' alt='glossier'/>
-                <div className={Style.link_box} onClick={()=> openTab ("https://youlookgood.netlify.app/")}><p>Link to</p></div>
-              </div>
-            </li>
-            <li>
-              <div className={Style.portfolio_text_box}>
-                <p className={Style.info_title}>seniorcorp (2인)</p>
-                <p className={Style.keyword}>반응형, html, css<br/>2024.05.20 ~ 2024.05.28</p>
-                <p className={Style.info_text}>고령화 사회에 시니어 세대를 위한 서비스를 제공하는 프로젝트를 제작했습니다. 저는 html과 css를 맡아 제작하였습니다.</p>
-              </div>
-              <div className={Style.img}>
-                <img src='./images/seniorcorp.png' alt='seniorcorp'/>
-                <div className={Style.link_box} onClick={()=> openTab ("https://seniorcorp.co.kr/")}><p>Link to</p></div>
-              </div>
-            </li> */}
           </ul>
         </div>
         <ul className={Style.tool_list}>
